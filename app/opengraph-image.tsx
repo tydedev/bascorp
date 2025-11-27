7103e9d6cbc1a54fad211c092aadd90e9b15afd4
Tydedev <basso.maria1@outlook.it>
2025-11-27 12:17:49 +0100

    added ImageResponse

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,12 +1,53 @@
-export const size = { width: 1200, height: 630 };
+import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-export default function Image() {
-  return (
-    <img
-      src="/og.png"
-      alt="Open Graph Image"
-      width={size.width}
-      height={size.height}
-    />
+// Image metadata
+export const alt = "Bascorp";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+  // Font loading, process.cwd() is Next.js project directory
+  const interSemiBold = await readFile(
+    join(process.cwd(), "assets/Inter-Bold.ttf")
+  );
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        About Acme
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
   );
 }
