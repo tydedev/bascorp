152a671a18c1cf56bf02970d5fe8b1e8d7391d46
Tydedev <basso.maria1@outlook.it>
2025-11-27 12:15:20 +0100

    generated static og

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,25 +1,12 @@
-import { ImageResponse } from "next/og";
-import { join } from "node:path";
-import { readFile } from "node:fs/promises";
+export const size = { width: 1200, height: 630 };
 
-export default async function Image() {
-  const logoData = await readFile(
-    join(process.cwd(), "public", "logo_dark.svg")
-  );
-  const logoSrc = Uint8Array.from(logoData).buffer;
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {/* @ts-expect-error Satori accepts ArrayBuffer/typed arrays for <img src> at runtime */}
-        <img src={logoSrc} height="100" width="100" />
-      </div>
-    )
+export default function Image() {
+  return (
+    <img
+      src="/og.png"
+      alt="Open Graph Image"
+      width={size.width}
+      height={size.height}
+    />
   );
 }
